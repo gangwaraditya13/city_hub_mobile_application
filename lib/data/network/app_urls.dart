d0462519206528a9b97c31a53d7d857e42d815d1
gangwaraditya13 <gangwaraditya13@gmail.com>
2026-02-11 00:10:30 +0530

    making mark on api call complete

--- lib/data/network/app_urls.dart
+++ lib/data/network/app_urls.dart
@@ -12,26 +12,26 @@ class AppUrls {
 
   ///user
   static String userInfoDetailsUrl = "${_baseUrl}/user/getuser";//
-  static String ownCityInfoUrl = "${_baseUrl}/user/own-city-info";
+  static String ownCityInfoUrl = "${_baseUrl}/user/own-city-info";//
   static String OtherCityInfoUrl = "${_baseUrl}/user/other-city-info";
-  static String userDetailUpdateUrl = "${_baseUrl}/user/update-user";
-  static String userPasswordUpdateUrl = "${_baseUrl}/user/update-password";
-  static String userDeleteUrl = "${_baseUrl}/user/delete-user";
+  static String userDetailUpdateUrl = "${_baseUrl}/user/update-user";//
+  static String userPasswordUpdateUrl = "${_baseUrl}/user/update-password";//
+  static String userDeleteUrl = "${_baseUrl}/user/delete-user";//
 
   ///image upload
-  static String uploadImage = "${_baseUrl}/user/image-upload";
+  static String uploadImage = "${_baseUrl}/user/image-upload";////
   ///set user pic to user
-  static String setUserProfilepicInUser = "${_baseUrl}/user/update-profile-image";
+  static String setUserProfilepicInUser = "${_baseUrl}/user/update-profile-image";//
   ///image delete of User pic
   static String deleteUserProfilePic = "${_baseUrl}/user/delete-profile-image";
 
   ///complaint
-  static String newComplaintUrl = "${_baseUrl}/complaint";
+  static String newComplaintUrl = "${_baseUrl}/complaint";//
 
-  static String updateComplaintUrl = "${_baseUrl}/complaint/update-complaint";
+  static String updateComplaintUrl = "${_baseUrl}/complaint/update-complaint";//
 
   static String deleteComplaint(String complaintId){
-    return "${_baseUrl}/complaint/delete-complaint/${complaintId}";
+    return "${_baseUrl}/complaint/delete-complaint/${complaintId}";//
   }
 
   static String getOneComplaint(String complaintId){
